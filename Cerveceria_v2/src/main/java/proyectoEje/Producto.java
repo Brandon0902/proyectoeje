65645744954c926a5b07961fd521a969e4a24321
Brandon0902 <115058579+Brandon0902@users.noreply.github.com>
2023-06-27 19:37:46 -0600

    Add files via upload

---
+++ Cerveceria_v2/src/main/java/proyectoEje/Producto.java
@@ -0,0 +1,7 @@
+
+package proyectoEje;
+
+
+public class Producto {
+    
+}
---
+++ Cerveceria_v2/src/main/java/proyectoEje/main.java
@@ -0,0 +1,11 @@
+
+
+package proyectoEje;
+
+
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Ponganse a Jalar");
+    }
+}
